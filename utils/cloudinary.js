2174bbbfb9a97e844d1582dc0af6de6c09bd9bd9
Parasnegi007 <psd89064@gmail.com>
2025-04-23 16:28:25 +0530

    new version

---
+++ utils/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: 'dwpvgqh74',
+  api_key: '349718361979178',
+  api_secret: '39Rg9pYYtP5CuFGqIorvdyA37MU',
+});
+
+module.exports = cloudinary;
